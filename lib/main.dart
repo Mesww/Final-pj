13cba084ae100ceb4882660b0a7aafd8b8b7bd33
Mesww <123399888+Mesww@users.noreply.github.com>
2024-02-20 20:24:12 +0700

    t

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:final_pj/pages/login/login.dart';
-import 'package:final_pj/provider/changeRoute.dart';
+// import 'package:final_pj/provider/changeRoute.dart';
 import 'package:final_pj/provider/users_list.dart';
 import 'package:flutter/material.dart';
 import 'package:final_pj/config/pallete.dart';
@@ -14,9 +14,11 @@ void main() {
       ChangeNotifierProvider(create: (context)=>Form_login()),
       ChangeNotifierProvider(create: (_) => usersProvider()),
       ChangeNotifierProvider(create: (_) => Register_provider()),
-    ChangeNotifierProvider(create: (_) => ChangeRoute())
+      ChangeNotifierProvider(create: (_) => ChangeRoute()),
     ],
-  ));
+    child: const MyApp(),
+  )
+  );
 }
 
 class MyApp extends StatelessWidget {
