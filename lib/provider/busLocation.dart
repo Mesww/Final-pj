aa8f243d2ee5eeb3f7b7681bccc7bb2a070a89b3
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-03-24 01:35:48 +0700

    1

--- lib/provider/busLocation.dart
+++ lib/provider/busLocation.dart
@@ -1,23 +1,43 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+const String bearerToken = 'k3wbpy57L4pVQC';
+
 
 class busLocation extends ChangeNotifier {
   final httpClient = http.Client();
-  late List<dynamic> busData;
-  Map<String, String> customHeaders = {
-    "Accept": "application/json",
-    "Content-Type": "application/json;charset=UTF-8"
-    "Authorization: Bearer k3wbpy57L4pVQC"
+
+Future<List<Bus>> fetchBus() async {
+  var url = Uri.parse('https://www.ppgps171.com/mobile/api/mfutransit');
+
+  final headers = {
+    'Authorization': 'Bearer $bearerToken',
+    'Content-Type': 'application/json',
   };
 
-  //get request
-  Future fetchData() async {
-    final Uri restAPIURL =
-        Uri.parse("https://www.ppgps171.com/mobile/api/mfutransit");
-    http.Response response = await httpClient.get(restAPIURL, headers: customHeaders);
-    final Map parsedData = await json.decode(response.body.toString());
-    busData = parsedData['data'];
-    print(busData);
+  var response = await http.get(url, headers: headers);
+
+  if (response.statusCode == 200) {
+    var jsonData = jsonDecode(response.body);
+    List<Bus> buses = [];
+    for (var key in jsonData['data'].keys) {
+      // Access data by key  (or any other key from the response)
+      final eachData = jsonData['data'][key];
+      final bus = Bus(
+        id: int.tryParse(key), // Attempt to convert key to int for id
+        direction: eachData['direction'],
+        speed: eachData['speed'],
+        serverTime: eachData['server_time'],
+        trackerTime: eachData['tracker_time'],
+        position: eachData['position'],
+      );
+      buses.add(bus);
+    }
+    return buses;
+  } else {
+    throw Exception('Failed to fetch bus data');
   }
 }
+}
+
+
