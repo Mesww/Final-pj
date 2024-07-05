12440c884d1594cf82382f994a2ab6890cb76026
Mesww <6431501102@lamduan.mfu.ac.th>
2024-07-05 14:53:36 +0700

    add jwt_decoder and add iosgoogleclientid in contrants
    note *** I changed uri in contrants for testing backend

--- lib/utils/contrants.dart
+++ lib/utils/contrants.dart
@@ -1,3 +1,5 @@
 class Constants {
-  static String uri = 'rv1wl6nk-4444.asse.devtunnels.ms';
+  // static String uri = 'rv1wl6nk-4444.asse.devtunnels.ms';
+  static String uri = 'http://localhost:4444';
+  static String iosgoogleclientid = "977400494676-r4ihi082v8uce61t9q3ofuuaia5pjare.apps.googleusercontent.com";
 }
