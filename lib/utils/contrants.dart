a37fe9166738f11ab3a430721f0323f44d3b1dd0
Mesww <6431501102@lamduan.mfu.ac.th>
2024-07-06 13:47:36 +0700

    edit server uri

--- lib/utils/contrants.dart
+++ lib/utils/contrants.dart
@@ -1,5 +1,8 @@
 class Constants {
   // static String uri = 'rv1wl6nk-4444.asse.devtunnels.ms';
-  static String uri = 'http://localhost:4444';
+  static String uri = 'http://172.25.51.100:8886';
   static String iosgoogleclientid = "977400494676-r4ihi082v8uce61t9q3ofuuaia5pjare.apps.googleusercontent.com";
+  static String androidgoogleclientid = "977400494676-lu73u24gvjk6tcsum144grfsoafra3vr.apps.googleusercontent.com";
+  static String servergoogleclientid = "977400494676-6nhvoga4j3ne04eb4un7024e5qj5tusl.apps.googleusercontent.com";
+
 }
